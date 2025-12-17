3db4866dbb1d878543b018cecf96371adcb97b35
tin-blip <tinsaefekadu554@gmail.com>
2025-12-17 15:25:25 +0300

    Create Longest common prefix.py

---
+++ Longest common prefix.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for i in range(len(prefix)):
+            for word in strs[1:]:
+                if i >= len(word) or word[i] != prefix[i]:
+                    return prefix[:i]
+        return prefix
